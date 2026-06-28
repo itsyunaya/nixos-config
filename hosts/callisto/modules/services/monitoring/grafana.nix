1a0046598e3991de80ee3e1a5d8e42cd598c3dc4
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-06-29 01:34:56 +0200

    fix(callisto): rotate grafana secret key and add to agenix

--- hosts/callisto/modules/services/monitoring/grafana.nix
+++ hosts/callisto/modules/services/monitoring/grafana.nix
@@ -1,11 +1,10 @@
-{
+{ config, ... }: {
 	services.grafana = {
 		enable = true;
 		openFirewall = true;
 
 		settings = {
-			# TODO: agenix
-			security.secret_key = "8eb1f7f00915b5b62f41dd5e23e412cdc0d6902887b3b73dd5fbc0e0951515bd";
+			security.secret_key = "$__file{${config.age.secrets.grafana_secret.path}}";
 			analytics.reporting_enabled = false;
 
 			server = {
--- hosts/callisto/secrets/default.nix
+++ hosts/callisto/secrets/default.nix
@@ -1,6 +1,15 @@
 {
 	age = {
 		identityPaths = [ "/home/callisto/.ssh/id_ed25519" ];
-		secrets.factorio.file = ./factorio.age;
+		secrets = {
+			factorio.file = ./factorio.age;
+			grafana_secret = {
+				file = ./grafana_secret.age;
+
+				owner = "grafana";
+				group = "grafana";
+				mode = "0400";
+			};
+		};
 	};
 }
